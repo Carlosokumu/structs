ac9fabe2fd7ffcb08424614d3e196d13f1e0e6a2
Carlosokumu <69672450+Carlosokumu@users.noreply.github.com>
2021-10-11 12:50:01 +0300

    started goroutines

--- routines/goroutines.go
+++ routines/goroutines.go
@@ -9,7 +9,26 @@ func hello() {
 	fmt.Println("Hello world goroutine")
 }
 func main() {
-	go hello()
-	time.Sleep(1 * time.Second)
-	fmt.Println("main function")
+
+	//go hello()
+	//time.Sleep(1 * time.Second)
+	//fmt.Println("main function")
+	start := time.Now()
+	//Self executing function
+	go func() {
+		for i := 0; i < 3; i++ {
+			fmt.Println(i)
+		}
+	}()
+	//self executing function
+	go func() {
+		for i := 0; i < 3; i++ {
+			fmt.Println(i)
+		}
+	}()
+	fmt.Println(start)
+	elapsed := time.Since(start)
+	fmt.Println("Total Time For Execution: " + elapsed.String())
+	time.Sleep(time.Minute)
+
 }
